94a96e4e2307b100357a3f35d33b6e7695108fa1
robivr <vrbarobi@gmail.com>
2023-02-21 19:11:30 +0100

    added genre list and updated sidebar

---
+++ src/components/Sidebar/Genre.tsx
@@ -0,0 +1,29 @@
+import { useState, useContext } from 'react';
+
+import GlobalContext from '../../store/global-store';
+
+interface GenreProps {
+  id: number;
+  name: string;
+}
+
+const Genre = ({ id, name }: GenreProps) => {
+  const [isActive, setIsActive] = useState(false);
+  const ctx = useContext(GlobalContext);
+
+  const toggleActiveState = () => {
+    setIsActive((currentlyActive) => !currentlyActive);
+    ctx.activateSearchButton();
+  };
+
+  return (
+    <li
+      className={`genre ${isActive ? 'active' : ''}`}
+      onClick={toggleActiveState}
+    >
+      {name}
+    </li>
+  );
+};
+
+export default Genre;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,23 +1,76 @@
-import React from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { AiOutlineRight } from 'react-icons/ai';
 import './Sidebar.css';
+import Genre from './Genre';
+import GlobalContext from '../../store/global-store';
+
+const genreUrl = 'https://api.themoviedb.org/3/genre/movie/list?language=en-US';
 
 const Sidebar = () => {
+  const [filtersOpen, setFiltersOpen] = useState(false);
+  const [genreList, setGenreList] = useState([]);
+  const ctx = useContext(GlobalContext);
+
+  useEffect(() => {
+    const fetchGenres = async () => {
+      const res = await fetch(
+        `${genreUrl}&api_key=${import.meta.env.VITE_API_KEY}`
+      );
+
+      const data = await res.json();
+
+      console.log(data);
+      setGenreList(data.genres);
+    };
+
+    fetchGenres();
+  }, []);
+
+  const toggleFilter = () => {
+    setFiltersOpen((filtersOpen) => !filtersOpen);
+  };
+
   return (
     <div className="sidebar">
       <div className="filter_card">
-        <span>Sort</span>
-        <AiOutlineRight />
+        <div className="card_name">
+          <span>Sort</span>
+          <span className="filter_icon">
+            <AiOutlineRight />
+          </span>
+        </div>
+      </div>
+      <div className={`filter_card ${filtersOpen ? 'filter_open' : ''}`}>
+        <div className="card_name" onClick={toggleFilter}>
+          <span>Filters</span>
+          <span className="filter_icon">
+            <AiOutlineRight />
+          </span>
+        </div>
+        <div className={`filter_card_bot ${filtersOpen ? 'filter_open' : ''}`}>
+          <h3>Genres</h3>
+          <ul className="genre-list">
+            {genreList.map((genre: any) => (
+              <Genre key={genre.id} {...genre} />
+            ))}
+          </ul>
+        </div>
       </div>
       <div className="filter_card">
-        Filters
-        <AiOutlineRight />
+        <div className="card_name">
+          <span>Where To Watch</span>
+          <span className="filter_icon">
+            <AiOutlineRight />
+          </span>
+        </div>
       </div>
-      <div className="filter_card">
-        <span>Where To Watch</span>
-        <AiOutlineRight />
+      <div
+        className={`filter_button ${
+          ctx.search_button_active ? '' : 'filter_button_inactive'
+        }`}
+      >
+        Search
       </div>
-      <div className="filter_button filter_button_inactive">Search</div>
     </div>
   );
 };
